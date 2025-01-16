ad37bccfd902f30436e310c87ca58300f7d95754
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-01-16 11:44:09 -0500

    update test

--- tools/test_pgm.sh
+++ tools/test_pgm.sh
@@ -3,6 +3,12 @@
 cd ..
 mvn clean package
 
+# Example test runs
 java -jar target/VisionGuard-1.0.jar --help
 
 java -Djava.awt.headless=true -jar target/VisionGuard-1.0.jar "input/" "MASK" "output/" "report/"
+
+java -Djava.awt.headless=true -jar target/VisionGuard-1.0.jar "src/main/resources/images/" "input/" "MASK" "output/" "report/" --ollama=true
+
+# Detects "medium text" image:
+java -Djava.awt.headless=true -jar target/VisionGuard-1.0.jar "src/main/resources/images/" "input/" "MASK" "output/" "report/" --minWidth=200 --minHeight=20 --maxWidth=250 --maxHeight=35
