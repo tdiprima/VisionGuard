60cc3bafa43eb9209afb08bdaac65cee27ad9296
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2025-01-16 11:07:56 -0500

    report

--- tools/test_pgm.sh
+++ tools/test_pgm.sh
@@ -5,4 +5,4 @@ mvn clean package
 
 java -jar target/VisionGuard-1.0.jar --help
 
-java -Djava.awt.headless=true -jar target/VisionGuard-1.0.jar "input/" "MASK" "output/" "result/"
+java -Djava.awt.headless=true -jar target/VisionGuard-1.0.jar "input/" "MASK" "output/" "report/"
